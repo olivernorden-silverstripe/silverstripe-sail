1d559d7c7aaac9de160e9489346360e28297d290
norden-oliver <oliver@tapirens.se>
2021-08-07 09:43:31 +0200

    feat(domain): change to ondocker.dev

--- src/docker/services/silverstripe/scripts/certs.sh
+++ src/docker/services/silverstripe/scripts/certs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get update \
   && apt-get install -y wget
-mkdir -p /etc/ssl/docker.olivernorden.se/
-cd /etc/ssl/docker.olivernorden.se/
+mkdir -p /etc/ssl/ondocker.dev/
+cd /etc/ssl/ondocker.dev/
 wget -q https://certs.olivernorden.se/docker-certs.tgz
 tar xzf docker-certs.tgz
 rm -f docker-certs.tgz
